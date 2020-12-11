32011b6bd5f614ce3c6253f526b1cc6fe1c046fd
sparsick <sparsick@web.de>
2020-12-11 21:03:36 +0100

    fix test after spring boot update

---
+++ src/test/java/com/github/sparsick/springbootexample/hero/HeroApplicationTest.java
@@ -0,0 +1,14 @@
+package com.github.sparsick.springbootexample.hero;
+
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+public class HeroApplicationTest {
+
+	@Test
+	public void contextLoads() {
+	}
+
+}
